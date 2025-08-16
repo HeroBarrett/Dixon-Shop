ed0be07cbc2a1479e34e20bc6563572d47a9c5ee
Dixon <510672083@qq.com>
2025-08-16 14:24:52 +0800

    轮播图-自动导入

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,8 +3,9 @@ import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
-  <CustomNavbar></CustomNavbar>
-  <view class="index">index</view>
+  <CustomNavbar />
+  <XtxSwiper />
+  <view class="index">index1</view>
 </template>
 
 <style lang="scss">
