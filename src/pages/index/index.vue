4c85e8028f662aa702f5be47066737e55fc05550
Dixon <510672083@qq.com>
2025-08-18 13:03:47 +0800

    前台分类获取数据和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,29 +1,45 @@
 <script setup lang="ts">
-import { getHomeBannerAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
-import type { BannerItem } from '@/types/home'
+import type { CategoryItem, BannerItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
 
+// 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
-
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
-  console.log(res)
+  // console.log(res)
   bannerList.value = res.result
+}
+
+// 获取前台分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategory = async () => {
+  const res = await getHomeCategoryAPI()
+  // console.log(res)
+  categoryList.value = res.result
 }
 
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategory()
 })
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
-  <view class="index">index1</view>
+  <!-- 分类面板 -->
+  <CategoryPanel :list="categoryList" />
+  <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
