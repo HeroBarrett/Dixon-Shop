4250c57d8c68726abbb1e0ce9d082b1953c88c9e
Dixon <510672083@qq.com>
2025-08-18 15:47:40 +0800

    猜你喜欢类型定义和列表渲染

--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,11 +1,15 @@
+import XtxGuess from '@/components/XtxGuess.vue'
+import XtxSwiper from '@/components/XtxSwiper.vue'
 /**
  * declare module '@vue/runtime-core'
  *   现调整为
  * declare module 'vue'
  */
+
 import 'vue'
 declare module 'vue' {
   export interface GlobalComponents {
-    //
+    XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
