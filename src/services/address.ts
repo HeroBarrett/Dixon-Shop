212c33ebe0347d4ed9ce67f9cdf3afdcd0c06880
Dixon <510672083@qq.com>
2025-08-22 15:35:18 +0800

    新建地址

---
+++ src/services/address.ts
@@ -0,0 +1,18 @@
+// 添加收货地址
+// POST
+// /member/address
+
+import type { AddressParams } from '@/types/address'
+import { http } from '@/utils/http'
+/**
+ * 添加收货地址
+ * @param data 请求参数
+ * @returns
+ */
+export const postMemberAddressAPI = (data: AddressParams) => {
+  return http({
+    method: 'POST',
+    url: '/member/address',
+    data,
+  })
+}
