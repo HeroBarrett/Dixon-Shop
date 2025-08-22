f56ebe8d94560f0ae0d0c855196d9205960c74fc
Dixon <510672083@qq.com>
2025-08-22 15:53:10 +0800

    地址管理-列表渲染

--- src/services/address.ts
+++ src/services/address.ts
@@ -2,7 +2,7 @@
 // POST
 // /member/address
 
-import type { AddressParams } from '@/types/address'
+import type { AddressItem, AddressParams } from '@/types/address'
 import { http } from '@/utils/http'
 /**
  * 添加收货地址
@@ -14,5 +14,20 @@ export const postMemberAddressAPI = (data: AddressParams) => {
     method: 'POST',
     url: '/member/address',
     data,
+  })
+}
+
+// 获取收货地址列表
+// GET
+// /member/address
+
+/**
+ * 获取收货地址列表
+ * @returns
+ */
+export const getMemberAddressAPI = () => {
+  return http<AddressItem[]>({
+    method: 'GET',
+    url: '/member/address',
   })
 }
