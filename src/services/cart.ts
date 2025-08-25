f418ba810f6f15d8c7be1aac73ad7ab0288c77d4
Dixon <510672083@qq.com>
2025-08-25 16:58:34 +0800

    购物车模块-列表渲染

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,3 +1,4 @@
+import type { CartItem } from '@/types/cart'
 import { http } from '@/utils/http'
 
 // 加入购物车
@@ -11,5 +12,17 @@ export const postMemberCartAPI = (data: { skuId: string; count: number }) => {
     method: 'POST',
     url: '/member/cart',
     data,
+  })
+}
+
+/**
+ * 获取购物车
+ * @param data 请求体参数
+ * @returns
+ */
+export const getMemberCartAPI = () => {
+  return http<CartItem[]>({
+    method: 'GET',
+    url: '/member/cart',
   })
 }
